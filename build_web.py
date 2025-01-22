01b6e04f62c6b64396e16aa955996eb006cc85ae
Anthony Metzidis <anthony.metzidis@gmail.com>
2025-01-21 16:06:35 -0800

    linux build compatibility

--- build_web.py
+++ build_web.py
@@ -1,32 +1,47 @@
 import os
 import subprocess
 from shutil import copytree, rmtree, copy2
-from os.path import join, isdir, basename, splitext
+from os.path import join, isdir, basename, splitext,isfile
+from sys import platform
 import hashlib
+import shutil
+Import("env")
 
+clean_dirs = ["data"]
 
 project_dir = os.getenv("PROJECT_DIR", os.getcwd())
 if not project_dir:
     raise ValueError("PROJECT_DIR environment variable is not set")
 
+if env.IsCleanTarget():
+    for d in clean_dirs:
+        abs_path = join(project_dir,d)
+        if isdir(abs_path):
+            print("rmdir: ", abs_path)
+            shutil.rmtree(abs_path)
+    Return()
 
 web_src_dir = join(project_dir, "src", "http_server", "axe-os")
 dist_dir = join(web_src_dir, "dist", "axe-os")
 data_dir = join(project_dir, "data")
 
-
-npm_path = "C:\\Program Files\\nodejs\\npm.cmd"
-
+# default to linux + darwin npm
+npm = "npm"
+if platform == "win32":
+    npm = "npm.cmd"
 
 if isdir(data_dir):
     rmtree(data_dir)
 
-
-subprocess.run([npm_path, "install"], cwd=web_src_dir, check=True)
-subprocess.run([npm_path, "run", "build"], cwd=web_src_dir, check=True)
-
+subprocess.run([npm, "install" ], cwd=web_src_dir, check=True)
+subprocess.run([npm, "run", "build"], cwd=web_src_dir, check=True)
 
 os.makedirs(data_dir)
+
+exclude_files = ["src/http_server/axe-os/node_modules/tempfile/node_modules/uuid/benchmark/benchmark-native.c"]
+for f in exclude_files:
+    if isfile(f):
+        os.remove(f)
 
 for root, _, files in os.walk(dist_dir):
     for file in files:
