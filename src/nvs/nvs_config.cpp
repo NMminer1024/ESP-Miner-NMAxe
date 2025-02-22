62a6f9ca2c6aef7ce2e0e82fac3439932d0e3237
NMminer1024 <nmminer1024@gmail.com>
2025-02-22 22:42:27 +0800

    fan invert true

--- src/nvs/nvs_config.cpp
+++ src/nvs/nvs_config.cpp
@@ -259,7 +259,7 @@ bool load_g_nmaxe(void){
     g_nmaxe.asic.frequency_req                  = nvs_config_get_u16(NVS_CONFIG_ASIC_FREQ, default_asic_frq);
     g_nmaxe.asic.vcore_req                      = nvs_config_get_u16(NVS_CONFIG_ASIC_VOLTAGE, default_asic_vcore);
     g_nmaxe.preference.fan.is_auto_speed        = nvs_config_get_u16(NVS_CONFIG_AUTO_FAN_SPEED, true);
-    g_nmaxe.preference.fan.invert_ploarity      = nvs_config_get_u16(NVS_CONFIG_INVERT_FAN_POLARITY, false); 
+    g_nmaxe.preference.fan.invert_ploarity      = nvs_config_get_u16(NVS_CONFIG_INVERT_FAN_POLARITY, true); 
     g_nmaxe.preference.fan.speed                = nvs_config_get_u16(NVS_CONFIG_FAN_SPEED, 90);
     g_nmaxe.preference.fan.self_test            = false;
     g_nmaxe.preference.led.indicator            = nvs_config_get_u8(NVS_CONFIG_LED_INDICATOR, true);
