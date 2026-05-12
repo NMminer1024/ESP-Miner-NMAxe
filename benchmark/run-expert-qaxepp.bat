5bcba4010de58e3e3e0c58f03e6ff29b63d2fc65
NMminer1024 <nmminer1024@gmail.com>
2026-05-12 13:49:29 +0800

    Add benchmark bat

---
+++ benchmark/run-expert-qaxepp.bat
@@ -0,0 +1,3 @@
+@echo off
+start "benchmark-22"  benchmark.exe -vr 1100,1300 -vs 25 -fr 440,750  -fs 25 -si 5 -bt 800 -ip 192.168.124.22
+start "benchmark-241" benchmark.exe -vr 1150,1450 -vs 25 -fr 540,1000 -f
