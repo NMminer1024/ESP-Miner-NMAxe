0fa7de35418276e8e5efaf13009528b215ecef77
NMminer1024 <nmminer1024@gmail.com>
2026-05-18 18:22:03 +0800

    2ph and 3ph passed

--- src/board/nmqaxepp.h
+++ src/board/nmqaxepp.h
@@ -13,14 +13,14 @@ inline BMxxx* create_qaxepp_asic_instance(HardwareSerial& serial, uint32_t baud,
 // 2-phase board (QAxe++): 0.005 Ω shunt, lower OC limit
 inline AxePowerHal* create_qaxepp_2ph_power_instance(axe_pwr_enable_pin_t en_pins, axe_pwr_adc_pin_t adc_pins, uint8_t vcore_regulator_pwm_pin, uint8_t pgood, uint8_t plug) {
     //                    phase, imax,  ifault,  sample reg
-    tps53647_cfg_t cfg = {2,      60,     90.0f,  0.005f};
+    tps53647_cfg_t cfg = {2,      60,     72.0f,  0.005f};
     return new TPS53647Class(en_pins, adc_pins, pgood, plug, cfg);
 }
 
 // 3-phase board (QAxe++ Rev6.1): 0.003 Ω shunt, higher OC limit
 inline AxePowerHal* create_qaxepp_3ph_power_instance(axe_pwr_enable_pin_t en_pins, axe_pwr_adc_pin_t adc_pins, uint8_t vcore_regulator_pwm_pin, uint8_t pgood, uint8_t plug) {
     //                    phase, imax,  ifault,  sample reg
-    tps53647_cfg_t cfg = {3,      120,    96.0f,    0.003f};
+    tps53647_cfg_t cfg = {3,      120,    100.0f,    0.003f};
     return new TPS53647Class(en_pins, adc_pins, pgood, plug, cfg);
 }
 #endif // __NMQAXEPP_BOARD_H_
